2f41c6f3ce9dc75a98789b0ca74b6902a7e6ddbf
CodedevAnkush <97466081+CodedevAnkush@users.noreply.github.com>
2026-07-14 16:21:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import functions_framework
 from flask import jsonify, make_response
 
 
-logger = logging.getLogger('travel_booking_fucntion')
+logger = logging.getLogger('travel_booking_function')
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
 handler.setFormatter(
@@ -15,7 +15,24 @@ if not logger.handlers:
     logger.addHandler(handler)
 
 @functions_framework.http
-def travel_booking(request):
+def travel_booking_cicd(request):
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
     logger.info('Applciation Start Complete')
 
-    return 'Application Exit'
+    logger.info('Extracting name from request')
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+
+    logger.info('Name extract process completed.')
+
+    logger.info( 'Application Exit')
+    return f"Hello {name}!"
+  
+
+    
